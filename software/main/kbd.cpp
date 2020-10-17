8498d27cf4dfea03855d1dbac1f3b36cb4c11c25
neo <sjkyspa@gmail.com>
2020-10-17 15:28:51 +0800

    build: add clang-format support

--- software/main/kbd.cpp
+++ software/main/kbd.cpp
@@ -1,19 +1,20 @@
 #include <stdio.h>
+
+#include <vector>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include <vector>
 
 extern "C" {
 void app_main();
 }
 
 void app_main() {
-    int i = 0;
-    std::vector<int> a;
-    while (1) {
-
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
-    }
+  int i = 0;
+  std::vector<int> a;
+  while (1) {
+    printf("[%d] Hello world!\n", i);
+    i++;
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+  }
 }
