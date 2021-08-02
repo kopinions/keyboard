055474467d36eb0b643bfa2a8952a8cece8effc1
neo <sjkyspa@gmail.com>
2021-08-02 18:45:31 +0800

    feat: pair with other device

--- software/components/ible/src/ble.cpp
+++ software/components/ible/src/ble.cpp
@@ -78,6 +78,21 @@ bt::ble::ble(const std::string& device_name, bt::appearance_t device_appearance,
   if (auto ret = esp_ble_gap_config_adv_data(&adv_data); ret != ESP_OK) {
     m_logger->error("set name failed", "set name failed");
   }
+
+  esp_ble_auth_req_t auth_req = ESP_LE_AUTH_BOND;     //bonding with peer device after authentication
+  esp_ble_io_cap_t iocap = ESP_IO_CAP_NONE;           //set the IO capability to No output No input
+  uint8_t key_size = 16;      //the key size should be 7~16 bytes
+  uint8_t init_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
+  uint8_t rsp_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
+  esp_ble_gap_set_security_param(ESP_BLE_SM_AUTHEN_REQ_MODE, &auth_req, sizeof(uint8_t));
+  esp_ble_gap_set_security_param(ESP_BLE_SM_IOCAP_MODE, &iocap, sizeof(uint8_t));
+  esp_ble_gap_set_security_param(ESP_BLE_SM_MAX_KEY_SIZE, &key_size, sizeof(uint8_t));
+  /* If your BLE device act as a Slave, the init_key means you hope which types of key of the master should distribute to you,
+  and the response key means which key you can distribute to the Master;
+  If your BLE device act as a master, the response key means you hope which types of key of the slave should distribute to you,
+  and the init key means which key you can distribute to the slave. */
+  esp_ble_gap_set_security_param(ESP_BLE_SM_SET_INIT_KEY, &init_key, sizeof(uint8_t));
+  esp_ble_gap_set_security_param(ESP_BLE_SM_SET_RSP_KEY, &rsp_key, sizeof(uint8_t));
 }
 void bt::ble::enable() {}
 
