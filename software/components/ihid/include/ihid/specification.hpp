0ae3c036b5c0721214a712bbf6010dd1520e73de
neo <sjkyspa@gmail.com>
2021-08-11 14:10:40 +0800

    feat: update the hid keycodes according USB HID Usage Tables Keyboard

--- software/components/ihid/include/ihid/specification.hpp
+++ software/components/ihid/include/ihid/specification.hpp
@@ -12,115 +12,153 @@ constexpr uint16_t HIDD_LE_REPORT_MAP_MAX_LEN = 512u;
 /// Length of Boot Report Char. Value Maximal Length
 constexpr uint16_t HIDD_LE_BOOT_REPORT_MAX_LEN = 8u;
 
-using hid_key_t = uint8_t;
-// HID Keyboard/Keypad Usage IDs (subset of the codes available in the USB HID Usage Tables spec)
-constexpr hid_key_t HID_KEY_RESERVED = 0u;       // No event inidicated
-constexpr hid_key_t HID_KEY_A = 4u;              // Keyboard a and A
-constexpr hid_key_t HID_KEY_B = 5u;              // Keyboard b and B
-constexpr hid_key_t HID_KEY_C = 6u;              // Keyboard c and C
-constexpr hid_key_t HID_KEY_D = 7u;              // Keyboard d and D
-constexpr hid_key_t HID_KEY_E = 8u;              // Keyboard e and E
-constexpr hid_key_t HID_KEY_F = 9u;              // Keyboard f and F
-constexpr hid_key_t HID_KEY_G = 10u;             // Keyboard g and G
-constexpr hid_key_t HID_KEY_H = 11u;             // Keyboard h and H
-constexpr hid_key_t HID_KEY_I = 12u;             // Keyboard i and I
-constexpr hid_key_t HID_KEY_J = 13u;             // Keyboard j and J
-constexpr hid_key_t HID_KEY_K = 14u;             // Keyboard k and K
-constexpr hid_key_t HID_KEY_L = 15u;             // Keyboard l and L
-constexpr hid_key_t HID_KEY_M = 16u;             // Keyboard m and M
-constexpr hid_key_t HID_KEY_N = 17u;             // Keyboard n and N
-constexpr hid_key_t HID_KEY_O = 18u;             // Keyboard o and O
-constexpr hid_key_t HID_KEY_P = 19u;             // Keyboard p and p
-constexpr hid_key_t HID_KEY_Q = 20u;             // Keyboard q and Q
-constexpr hid_key_t HID_KEY_R = 21u;             // Keyboard r and R
-constexpr hid_key_t HID_KEY_S = 22u;             // Keyboard s and S
-constexpr hid_key_t HID_KEY_T = 23u;             // Keyboard t and T
-constexpr hid_key_t HID_KEY_U = 24u;             // Keyboard u and U
-constexpr hid_key_t HID_KEY_V = 25u;             // Keyboard v and V
-constexpr hid_key_t HID_KEY_W = 26u;             // Keyboard w and W
-constexpr hid_key_t HID_KEY_X = 27u;             // Keyboard x and X
-constexpr hid_key_t HID_KEY_Y = 28u;             // Keyboard y and Y
-constexpr hid_key_t HID_KEY_Z = 29u;             // Keyboard z and Z
-constexpr hid_key_t HID_KEY_1 = 30u;             // Keyboard 1 and !
-constexpr hid_key_t HID_KEY_2 = 31u;             // Keyboard 2 and @
-constexpr hid_key_t HID_KEY_3 = 32u;             // Keyboard 3 and #
-constexpr hid_key_t HID_KEY_4 = 33u;             // Keyboard 4 and %
-constexpr hid_key_t HID_KEY_5 = 34u;             // Keyboard 5 and %
-constexpr hid_key_t HID_KEY_6 = 35u;             // Keyboard 6 and ^
-constexpr hid_key_t HID_KEY_7 = 36u;             // Keyboard 7 and &
-constexpr hid_key_t HID_KEY_8 = 37u;             // Keyboard 8 and *
-constexpr hid_key_t HID_KEY_9 = 38u;             // Keyboard 9 and (
-constexpr hid_key_t HID_KEY_0 = 39u;             // Keyboard 0 and )
-constexpr hid_key_t HID_KEY_RETURN = 40u;        // Keyboard Return (ENTER)
-constexpr hid_key_t HID_KEY_ESCAPE = 41u;        // Keyboard ESCAPE
-constexpr hid_key_t HID_KEY_DELETE = 42u;        // Keyboard DELETE (Backspace)
-constexpr hid_key_t HID_KEY_TAB = 43u;           // Keyboard Tab
-constexpr hid_key_t HID_KEY_SPACEBAR = 44u;      // Keyboard Spacebar
-constexpr hid_key_t HID_KEY_MINUS = 45u;         // Keyboard - and (underscore)
-constexpr hid_key_t HID_KEY_EQUAL = 46u;         // Keyboard = and +
-constexpr hid_key_t HID_KEY_LEFT_BRKT = 47u;     // Keyboard [ and {
-constexpr hid_key_t HID_KEY_RIGHT_BRKT = 48u;    // Keyboard ] and }
-constexpr hid_key_t HID_KEY_BACK_SLASH = 49u;    // Keyboard \ and |
-constexpr hid_key_t HID_KEY_SEMI_COLON = 51u;    // Keyboard ; and :
-constexpr hid_key_t HID_KEY_SGL_QUOTE = 52u;     // Keyboard ' and "
-constexpr hid_key_t HID_KEY_GRV_ACCENT = 53u;    // Keyboard Grave Accent and Tilde
-constexpr hid_key_t HID_KEY_COMMA = 54u;         // Keyboard , and <
-constexpr hid_key_t HID_KEY_DOT = 55u;           // Keyboard . and >
-constexpr hid_key_t HID_KEY_FWD_SLASH = 56u;     // Keyboard / and ?
-constexpr hid_key_t HID_KEY_CAPS_LOCK = 57u;     // Keyboard Caps Lock
-constexpr hid_key_t HID_KEY_F1 = 58u;            // Keyboard F1
-constexpr hid_key_t HID_KEY_F2 = 59u;            // Keyboard F2
-constexpr hid_key_t HID_KEY_F3 = 60u;            // Keyboard F3
-constexpr hid_key_t HID_KEY_F4 = 61u;            // Keyboard F4
-constexpr hid_key_t HID_KEY_F5 = 62u;            // Keyboard F5
-constexpr hid_key_t HID_KEY_F6 = 63u;            // Keyboard F6
-constexpr hid_key_t HID_KEY_F7 = 64u;            // Keyboard F7
-constexpr hid_key_t HID_KEY_F8 = 65u;            // Keyboard F8
-constexpr hid_key_t HID_KEY_F9 = 66u;            // Keyboard F9
-constexpr hid_key_t HID_KEY_F10 = 67u;           // Keyboard F10
-constexpr hid_key_t HID_KEY_F11 = 68u;           // Keyboard F11
-constexpr hid_key_t HID_KEY_F12 = 69u;           // Keyboard F12
-constexpr hid_key_t HID_KEY_PRNT_SCREEN = 70u;   // Keyboard Print Screen
-constexpr hid_key_t HID_KEY_SCROLL_LOCK = 71u;   // Keyboard Scroll Lock
-constexpr hid_key_t HID_KEY_PAUSE = 72u;         // Keyboard Pause
-constexpr hid_key_t HID_KEY_INSERT = 73u;        // Keyboard Insert
-constexpr hid_key_t HID_KEY_HOME = 74u;          // Keyboard Home
-constexpr hid_key_t HID_KEY_PAGE_UP = 75u;       // Keyboard PageUp
-constexpr hid_key_t HID_KEY_DELETE_FWD = 76u;    // Keyboard Delete Forward
-constexpr hid_key_t HID_KEY_END = 77u;           // Keyboard End
-constexpr hid_key_t HID_KEY_PAGE_DOWN = 78u;     // Keyboard PageDown
-constexpr hid_key_t HID_KEY_RIGHT_ARROW = 79u;   // Keyboard RightArrow
-constexpr hid_key_t HID_KEY_LEFT_ARROW = 80u;    // Keyboard LeftArrow
-constexpr hid_key_t HID_KEY_DOWN_ARROW = 81u;    // Keyboard DownArrow
-constexpr hid_key_t HID_KEY_UP_ARROW = 82u;      // Keyboard UpArrow
-constexpr hid_key_t HID_KEY_NUM_LOCK = 83u;      // Keypad Num Lock and Clear
-constexpr hid_key_t HID_KEY_DIVIDE = 84u;        // Keypad /
-constexpr hid_key_t HID_KEY_MULTIPLY = 85u;      // Keypad *
-constexpr hid_key_t HID_KEY_SUBTRACT = 86u;      // Keypad -
-constexpr hid_key_t HID_KEY_ADD = 87u;           // Keypad +
-constexpr hid_key_t HID_KEY_ENTER = 88u;         // Keypad ENTER
-constexpr hid_key_t HID_KEYPAD_1 = 89u;          // Keypad 1 and End
-constexpr hid_key_t HID_KEYPAD_2 = 90u;          // Keypad 2 and Down Arrow
-constexpr hid_key_t HID_KEYPAD_3 = 91u;          // Keypad 3 and PageDn
-constexpr hid_key_t HID_KEYPAD_4 = 92u;          // Keypad 4 and Lfet Arrow
-constexpr hid_key_t HID_KEYPAD_5 = 93u;          // Keypad 5
-constexpr hid_key_t HID_KEYPAD_6 = 94u;          // Keypad 6 and Right Arrow
-constexpr hid_key_t HID_KEYPAD_7 = 95u;          // Keypad 7 and Home
-constexpr hid_key_t HID_KEYPAD_8 = 96u;          // Keypad 8 and Up Arrow
-constexpr hid_key_t HID_KEYPAD_9 = 97u;          // Keypad 9 and PageUp
-constexpr hid_key_t HID_KEYPAD_0 = 98u;          // Keypad 0 and Insert
-constexpr hid_key_t HID_KEYPAD_DOT = 99u;        // Keypad . and Delete
-constexpr hid_key_t HID_KEY_MUTE = 127u;         // Keyboard Mute
-constexpr hid_key_t HID_KEY_VOLUME_UP = 128u;    // Keyboard Volume up
-constexpr hid_key_t HID_KEY_VOLUME_DOWN = 129u;  // Keyboard Volume down
-constexpr hid_key_t HID_KEY_LEFT_CTRL = 224u;    // Keyboard LeftContorl
-constexpr hid_key_t HID_KEY_LEFT_SHIFT = 225u;   // Keyboard LeftShift
-constexpr hid_key_t HID_KEY_LEFT_ALT = 226u;     // Keyboard LeftAlt
-constexpr hid_key_t HID_KEY_LEFT_GUI = 227u;     // Keyboard LeftGUI
-constexpr hid_key_t HID_KEY_RIGHT_CTRL = 228u;   // Keyboard LeftContorl
-constexpr hid_key_t HID_KEY_RIGHT_SHIFT = 229u;  // Keyboard LeftShift
-constexpr hid_key_t HID_KEY_RIGHT_ALT = 230u;    // Keyboard LeftAlt
-constexpr hid_key_t HID_KEY_RIGHT_GUI = 231u;    // Keyboard RightGUI
+enum key_t : uint8_t {
+  NONE,
+  // modifier
+  FN,
+  LCTRL = 0xE0,
+  LSHIFT = 0xE1,
+  LALT = 0xE2,
+  LGUI = 0xE3,
+  RCTRL = 0xE4,
+  RSHIFT = 0xE5,
+  RALT = 0xE6,
+  RGUI = 0xE7,
+  // key
+  A = 0x4,
+  B = 0x5,
+  C = 0x6,
+  D = 0x7,
+  E = 0x8,
+  F = 0x9,
+  G = 0x0A,
+  H = 0x0B,
+  I = 0x0C,
+  J = 0x0D,
+  K = 0x0E,
+  L = 0x0F,
+  M = 0x10,
+  N = 0x11,
+  O = 0x12,
+  P = 0x13,
+  Q = 0x14,
+  R = 0x15,
+  S = 0x16,
+  T = 0x17,
+  U = 0x18,
+  V = 0x19,
+  W = 0x1A,
+  X = 0x1B,
+  Y = 0x1C,
+  Z = 0x1D,
+  K1 = 0x1E,
+  K2 = 0x1F,
+  K3 = 0x20,
+  K4 = 0x21,
+  K5 = 0x22,
+  K6 = 0x23,
+  K7 = 0x24,
+  K8 = 0x25,
+  K9 = 0x26,
+  K0 = 0x27,
+  ENTER = 0x28,
+  ESC = 0x29,
+  BACKSPACE = 0x2A,
+  TAB = 0x2B,
+  SPACE = 0x2C,
+  MINUS = 0x2D,
+  EQUAL = 0x2E,
+  LBRACE = 0x2F,
+  RBRACE = 0x30,
+  BACKSLASH = 0x31,
+  NON_US_NUM = 0x32,
+  SEMICOLON = 0x33,
+  QUOTE = 0x34,
+  TILDE = 0x35,
+  COMMA = 0x36,
+  PERIOD = 0x37,
+  SLASH = 0x38,
+  CAPS_LOCK = 0x39,
+  // functional key
+  F1 = 0x3A,
+  F2 = 0x3B,
+  F3 = 0x3C,
+  F4 = 0x3D,
+  F5 = 0x3E,
+  F6 = 0x3F,
+  F7 = 0x40,
+  F8 = 0x41,
+  F9 = 0x42,
+  F10 = 0x43,
+  F11 = 0x44,
+  F12 = 0x45,
+  F13 = 0x68,
+  F14 = 0x69,
+  F15 = 0x6A,
+  F16 = 0x6B,
+  F17 = 0x6C,
+  F18 = 0x6D,
+  F19 = 0x6E,
+  F20 = 0x6F,
+  F21 = 0x70,
+  F22 = 0x71,
+  F23 = 0x72,
+  F24 = 0x73,
+  PRINTSCREEN = 0x46,
+  SCROLL_LOCK = 0x47,
+  PAUSE_BREAK = 0x48,
+  INSERT = 0x49,
+  HOME = 0x4A,
+  PAGE_UP = 0x4B,
+  DELETE = 0x4C,
+  END = 0x4D,
+  PAGE_DOWN = 0x4E,
+  RIGHT = 0x4F,
+  LEFT = 0x50,
+  DOWN = 0x51,
+  UP = 0x52,
+  NUM_LOCK = 0x53,
+  MENU = 0x65,
+  // num pads
+  PAD_SLASH = 0x54,
+  PAD_ASTERIX = 0x55,
+  PAD_MINUS = 0x56,
+  PAD_PLUS = 0x57,
+  PAD_ENTER = 0x58,
+  PAD_1 = 0x59,
+  PAD_2 = 0x5A,
+  PAD_3 = 0x5B,
+  PAD_4 = 0x5C,
+  PAD_5 = 0x5D,
+  PAD_6 = 0x5E,
+  PAD_7 = 0x5F,
+  PAD_8 = 0x60,
+  PAD_9 = 0x61,
+  PAD_0 = 0x62,
+  PAD_PERIOD = 0x63,
+  PAD_NON_US_NUM = 0x64,
+  PAD_EQUAL = 0x67,
+  // media key
+  PLAY,
+  PAUSE,
+  RECORD,
+  FAST_FORWARD,
+  REWIND,
+  NEXT_TRACK,
+  PREV_TRACK,
+  STOP,
+  EJECT,
+  RANDOM_PLAY,
+  PLAY_PAUSE,
+  PLAY_SKIP,
+  MUTE = 0x7F,
+  VOLUME_INC = 0x80,
+  VOLUME_DEC = 0x81,
+  // system key
+  POWER_DOWN = 0x66,
+  SLEEP,
+  WAKE_UP,
+};
 
 /// HIDD Features structure
 struct feature_t {
