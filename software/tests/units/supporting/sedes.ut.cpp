eb8764d8e5deb2f1018b2d9b2fafb1f53eb59895
neo <sjkyspa@gmail.com>
2021-08-20 15:59:35 +0800

    feat: should abel to handle the special key release

--- software/tests/units/supporting/sedes.ut.cpp
+++ software/tests/units/supporting/sedes.ut.cpp
@@ -24,4 +24,17 @@ int main() {
 
     expect_that<uint8_t>(*buf, matchers::eq((uint8_t)0b01000010));
   };
+
+
+  "should_serialize_to_0x00_when_ralt_lshift_released"_test = [] {
+    std::vector<kopinions::key_t> keys{
+        {kopinions::key_t::id_t::RALT, kopinions::key_t::status_t::RELEASED},
+        {kopinions::key_t::id_t::LSHIFT, kopinions::key_t::status_t::RELEASED},
+    };
+    auto *s = new sedes{};
+    const std::unique_ptr<uint8_t[]> &ptr = s->serialize(keys);
+    uint8_t *buf = ptr.get();
+
+    expect_that<uint8_t>(*buf, matchers::eq((uint8_t)0x00));
+  };
 }
