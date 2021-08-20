e0f26096b7fbf58d8b2b8eea245f92f52e142bd2
neo <sjkyspa@gmail.com>
2021-08-20 18:36:43 +0800

    refactor: make the modifiers member variable

--- software/components/supporting/include/supporting/sedes.hpp
+++ software/components/supporting/include/supporting/sedes.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <map>
 #include <memory>
 #include <vector>
 
@@ -8,4 +9,12 @@
 class sedes {
  public:
   std::unique_ptr<uint8_t[]> serialize(const std::vector<kopinions::key_t> &);
+
+ private:
+  const std::map<kopinions::key_t::id_t, std::uint8_t> m_modifiers = {
+      {kopinions::key_t::id_t::LCTRL, 0x01}, {kopinions::key_t::id_t::LSHIFT, 0x02},
+      {kopinions::key_t::id_t::LALT, 0x04},  {kopinions::key_t::id_t::LGUI, 0x08},
+      {kopinions::key_t::id_t::RCTRL, 0x10}, {kopinions::key_t::id_t::RSHIFT, 0x20},
+      {kopinions::key_t::id_t::RALT, 0x40},  {kopinions::key_t::id_t::RGUI, 0x80},
+  };
 };
--- software/components/supporting/src/sedes.cpp
+++ software/components/supporting/src/sedes.cpp
@@ -1,19 +1,13 @@
 #include "supporting/sedes.hpp"
 
-#include <map>
 
-static const std::map<kopinions::key_t::id_t, std::uint8_t> modifiers = {
-    {kopinions::key_t::id_t::LCTRL, 0x01}, {kopinions::key_t::id_t::LSHIFT, 0x02},
-    {kopinions::key_t::id_t::LALT, 0x04},  {kopinions::key_t::id_t::LGUI, 0x08},
-    {kopinions::key_t::id_t::RCTRL, 0x10}, {kopinions::key_t::id_t::RSHIFT, 0x20},
-    {kopinions::key_t::id_t::RALT, 0x40},  {kopinions::key_t::id_t::RGUI, 0x80},
-};
+
 
 std::unique_ptr<uint8_t[]> sedes::serialize(const std::vector<kopinions::key_t>& keys) {
   auto chars = std::unique_ptr<uint8_t[]>(new uint8_t[8]);
   int cursor = 2;
   for (auto k : keys) {
-    if (auto it = modifiers.find(k.id); it != modifiers.end()) {
+    if (auto it = m_modifiers.find(k.id); it != m_modifiers.end()) {
       auto [_, mask] = *it;
       if (k.sts == kopinions::key_t::status_t::PRESSED) {
         chars[0] |= mask;
