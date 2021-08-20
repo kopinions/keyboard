ec3ae76ff8be9024f31c9fe0da33389211358570
neo <sjkyspa@gmail.com>
2021-08-20 18:35:23 +0800

    feat: should able to serial the normal keys

--- software/components/supporting/src/sedes.cpp
+++ software/components/supporting/src/sedes.cpp
@@ -1,23 +1,26 @@
 #include "supporting/sedes.hpp"
 
-// RGUI#RALT#RSHIFT#RCTRL#LGUI#LALT#LSHFT#LCTRL
-constexpr kopinions::key_t::id_t specials[8] = {
-    kopinions::key_t::id_t::LCTRL, kopinions::key_t::id_t::LSHIFT, kopinions::key_t::id_t::LALT,
-    kopinions::key_t::id_t::LGUI,  kopinions::key_t::id_t::RCTRL,  kopinions::key_t::id_t::RSHIFT,
-    kopinions::key_t::id_t::RALT,  kopinions::key_t::id_t::RGUI,
+#include <map>
+
+static const std::map<kopinions::key_t::id_t, std::uint8_t> modifiers = {
+    {kopinions::key_t::id_t::LCTRL, 0x01}, {kopinions::key_t::id_t::LSHIFT, 0x02},
+    {kopinions::key_t::id_t::LALT, 0x04},  {kopinions::key_t::id_t::LGUI, 0x08},
+    {kopinions::key_t::id_t::RCTRL, 0x10}, {kopinions::key_t::id_t::RSHIFT, 0x20},
+    {kopinions::key_t::id_t::RALT, 0x40},  {kopinions::key_t::id_t::RGUI, 0x80},
 };
-#include <iostream>
+
 std::unique_ptr<uint8_t[]> sedes::serialize(const std::vector<kopinions::key_t>& keys) {
   auto chars = std::unique_ptr<uint8_t[]>(new uint8_t[8]);
-
+  int cursor = 2;
   for (auto k : keys) {
-    uint8_t mask{0};
-    for (auto i = 0u; i < 8; i++) {
-      if (specials[i] == k.id && k.sts == kopinions::key_t::status_t::PRESSED) {
-        mask |= (1 << i);
+    if (auto it = modifiers.find(k.id); it != modifiers.end()) {
+      auto [_, mask] = *it;
+      if (k.sts == kopinions::key_t::status_t::PRESSED) {
+        chars[0] |= mask;
       }
+    } else {
+      chars[cursor++] = static_cast<uint8_t>(k.id);
     }
-    chars[0] |= mask;
   }
   return std::move(chars);
 }
--- software/tests/units/supporting/sedes.ut.cpp
+++ software/tests/units/supporting/sedes.ut.cpp
@@ -25,7 +25,6 @@ int main() {
     expect_that<uint8_t>(*buf, matchers::eq((uint8_t)0b01000010));
   };
 
-
   "should_serialize_to_0x00_when_ralt_lshift_released"_test = [] {
     std::vector<kopinions::key_t> keys{
         {kopinions::key_t::id_t::RALT, kopinions::key_t::status_t::RELEASED},
@@ -36,5 +35,16 @@ int main() {
     uint8_t *buf = ptr.get();
 
     expect_that<uint8_t>(*buf, matchers::eq((uint8_t)0x00));
+  };
+
+  "should_serialize_the_third_byte_to_0x04_when_key_is_a"_test = [] {
+    std::vector<kopinions::key_t> keys{
+        {kopinions::key_t::id_t::A, kopinions::key_t::status_t::PRESSED},
+    };
+    auto *s = new sedes{};
+    const std::unique_ptr<uint8_t[]> &ptr = s->serialize(keys);
+    uint8_t *buf = ptr.get();
+
+    expect_that<uint8_t>(*(buf+2), matchers::eq((uint8_t)0x04));
   };
 }
